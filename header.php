57a512a9466e9ef7b6482694d626a2f7959f54b5
Animesh702 <animesh.infotechsolz@gmail.com>
2022-10-11 16:05:29 +0530

    changes

--- header.php
+++ header.php
@@ -4,264 +4,261 @@ error_reporting(0);
 <!-- Header -->
 
 <header class="header-v4">
-		<!-- Header desktop -->
-		<div class="container-menu-desktop">
-			<!-- Topbar -->
-			<div class="top-bar">
-				<div class="content-topbar flex-sb-m h-full container">
-					<div class="left-top-bar">
-						Free shipping for standard order over $100
-					</div>
+    <!-- Header desktop -->
+    <div class="container-menu-desktop">
+        <!-- Topbar -->
+        <div class="top-bar">
+            <div class="content-topbar flex-sb-m h-full container">
+                <div class="left-top-bar">
+                    Free shipping for standard order over $100
+                </div>
 
-					<div class="right-top-bar flex-w h-full">
-						<a href="#" class="flex-c-m trans-04 p-lr-25">
-							Help & FAQs
-						</a>
+                <div class="right-top-bar flex-w h-full">
+                    <a href="#" class="flex-c-m trans-04 p-lr-25">
+                        Help & FAQs
+                    </a>
 
-						<a href="#" class="flex-c-m trans-04 p-lr-25">
-							My Account
-						</a>
+                    <a href="#" class="flex-c-m trans-04 p-lr-25">
+                        My Account
+                    </a>
 
-						<a href="#" class="flex-c-m trans-04 p-lr-25">
-							EN
-						</a>
+                    <a href="#" class="flex-c-m trans-04 p-lr-25">
+                        EN
+                    </a>
 
-						<a href="#" class="flex-c-m trans-04 p-lr-25">
-							USD
-						</a>
-					</div>
-				</div>
-			</div>
+                    <a href="#" class="flex-c-m trans-04 p-lr-25">
+                        USD
+                    </a>
+                </div>
+            </div>
+        </div>
 
-			<div class="wrap-menu-desktop how-shadow1">
-				<nav class="limiter-menu-desktop container">
-					
-					<!-- Logo desktop -->		
-					<a href="#" class="logo">
-						<img src="images/icons/logo-01.png" alt="IMG-LOGO">
-					</a>
+        <div class="wrap-menu-desktop how-shadow1">
+            <nav class="limiter-menu-desktop container">
 
-					<!-- Menu desktop -->
-					<div class="menu-desktop">
-						<ul class="main-menu">
-							<li>
-								<a href="shop.php">Home</a>
-								<ul class="sub-menu">
-									<li><a href="index.html">Homepage 1</a></li>
-									<li><a href="home-02.html">Homepage 2</a></li>
-									<li><a href="home-03.html">Homepage 3</a></li>
-								</ul>
-							</li>
+                <!-- Logo desktop -->
+                <a href="#" class="logo">
+                    <img src="images/icons/logo-01.png" alt="IMG-LOGO">
+                </a>
 
-							<li class="active-menu">
-								<a href="index.php">Shop</a>
-							</li>
+                <!-- Menu desktop -->
+                <div class="menu-desktop">
+                    <ul class="main-menu">
+                        <li>
+                            <a href="shop.php">Home</a>
+                            <ul class="sub-menu">
+                                <li><a href="index.html">Homepage 1</a></li>
+                                <li><a href="home-02.html">Homepage 2</a></li>
+                                <li><a href="home-03.html">Homepage 3</a></li>
+                            </ul>
+                        </li>
 
-							<li class="label1" data-label1="hot">
-								<a href="shoping-cart.html">Features</a>
-							</li>
+                        <li class="active-menu">
+                            <a href="index.php">Shop</a>
+                        </li>
 
-							<li>
-								<a href="blog.html">Blog</a>
-							</li>
+                        <li class="label1" data-label1="hot">
+                            <a href="shoping-cart.html">Features</a>
+                        </li>
 
-							<li>
-								<a href="about.html">About</a>
-							</li>
+                        <li>
+                            <a href="blog.html">Blog</a>
+                        </li>
 
-							<li>
-								<a href="contact.html">Contact</a>
-							</li>
-						</ul>
-					</div>	
+                        <li>
+                            <a href="about.html">About</a>
+                        </li>
+
+                        <li>
+                            <a href="contact.html">Contact</a>
+                        </li>
+                    </ul>
+                </div>
 
 
-					<!-- Icon header -->
-					<div class="wrap-icon-header flex-w flex-r-m">
+                <!-- Icon header -->
+                <div class="wrap-icon-header flex-w flex-r-m">
 
-						<div class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 js-show-modal-search">
-							<i class="zmdi zmdi-search"></i>
-						</div>
-						<div><a href="my_order.php">My Orders</a></div>
+                    <div class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 js-show-modal-search">
+                        <i class="zmdi zmdi-search"></i>
+                    </div>
+                    <div><a href="my_order.php">My Orders</a></div>
 
-						<?php
+                    <?php
 						$id=$_SESSION['user'];
 						if($id == "")
 						{
 						?>
-						<div class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11  js-show-cart" >
-							<i class="zmdi zmdi-shopping-cart">
-							</i>
-						</div>
+                    <div class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11  js-show-cart">
+                        <i class="zmdi zmdi-shopping-cart">
+                        </i>
+                    </div>
 
-						<a href="#" class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 p-l-10 ">
-							<i class="zmdi zmdi-favorite-outline"></i>
-						</a>
-						<a href="user_login.php">Login</a><?php
+                    <a href="#" class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 p-l-10 ">
+                        <i class="zmdi zmdi-favorite-outline"></i>
+                    </a>
+                    <a href="user_login.php">Login</a><?php
 						}
 						else{
 							?>
-							<div class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11  js-show-cart" >
-							<i class="zmdi zmdi-shopping-cart" id="cartitem" >
-							</i>
-						</div>
+                    <div class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11  js-show-cart">
+                        <i class="zmdi zmdi-shopping-cart" id="cartitem">
+                        </i>
+                    </div>
 
-						<a href="#" class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 p-l-10 ">
-							<i class="zmdi zmdi-favorite-outline" id="wishitem"></i>
-						</a><a href="logout.php">Logout</a><?php
+                    <a href="#" class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 p-l-10 ">
+                        <i class="zmdi zmdi-favorite-outline" id="wishitem"></i>
+                    </a><a href="logout.php">Logout</a><?php
 						}
 						?>
-					</div>
+                </div>
 
-				</nav>
-			</div>	
-		</div>
+            </nav>
+        </div>
+    </div>
 
-		<!-- Header Mobile -->
-		<div class="wrap-header-mobile">
-			<!-- Logo moblie -->		
-			<div class="logo-mobile">
-				<a href="index.html"><img src="images/icons/logo-01.png" alt="IMG-LOGO"></a>
-			</div>
+    <!-- Header Mobile -->
+    <div class="wrap-header-mobile">
+        <!-- Logo moblie -->
+        <div class="logo-mobile">
+            <a href="index.html"><img src="images/icons/logo-01.png" alt="IMG-LOGO"></a>
+        </div>
 
-			<!-- Icon header -->
-			<div class="wrap-icon-header flex-w flex-r-m m-r-15">
-				<div class="icon-header-item cl2 hov-cl1 trans-04 p-r-11 js-show-modal-search">
-					<i class="zmdi zmdi-search"></i>
-				</div>
+        <!-- Icon header -->
+        <div class="wrap-icon-header flex-w flex-r-m m-r-15">
+            <div class="icon-header-item cl2 hov-cl1 trans-04 p-r-11 js-show-modal-search">
+                <i class="zmdi zmdi-search"></i>
+            </div>
 
-				<div  class="icon-header-item cl2 hov-cl1 trans-04 p-r-11 p-l-10 icon-header-noti js-show-cart">
-					<i class="zmdi zmdi-shopping-cart" ></i>
-				</div>
+            <div class="icon-header-item cl2 hov-cl1 trans-04 p-r-11 p-l-10 icon-header-noti js-show-cart">
+                <i class="zmdi zmdi-shopping-cart"></i>
+            </div>
 
-				<a href="#" class="dis-block icon-header-item cl2 hov-cl1 trans-04 p-r-11 p-l-10"  >
-					<i class="zmdi zmdi-favorite-outline" ></i>
-				</a>
+            <a href="#" class="dis-block icon-header-item cl2 hov-cl1 trans-04 p-r-11 p-l-10">
+                <i class="zmdi zmdi-favorite-outline"></i>
+            </a>
 
-			</div>
+        </div>
 
-			<!-- Button show menu -->
-			<div class="btn-show-menu-mobile hamburger hamburger--squeeze">
-				<span class="hamburger-box">
-					<span class="hamburger-inner"></span>
-				</span>
-			</div>
-		</div>
+        <!-- Button show menu -->
+        <div class="btn-show-menu-mobile hamburger hamburger--squeeze">
+            <span class="hamburger-box">
+                <span class="hamburger-inner"></span>
+            </span>
+        </div>
+    </div>
 
 
-		<!-- Menu Mobile -->
-		<div class="menu-mobile">
-			<ul class="topbar-mobile">
-				<li>
-					<div class="left-top-bar">
-						Free shipping for standard order over $100
-					</div>
-				</li>
+    <!-- Menu Mobile -->
+    <div class="menu-mobile">
+        <ul class="topbar-mobile">
+            <li>
+                <div class="left-top-bar">
+                    Free shipping for standard order over $100
+                </div>
+            </li>
 
-				<li>
-					<div class="right-top-bar flex-w h-full">
-						<a href="#" class="flex-c-m p-lr-10 trans-04">
-							Help & FAQs
-						</a>
+            <li>
+                <div class="right-top-bar flex-w h-full">
+                    <a href="#" class="flex-c-m p-lr-10 trans-04">
+                        Help & FAQs
+                    </a>
 
-						<a href="#" class="flex-c-m p-lr-10 trans-04">
-							My Account
-						</a>
+                    <a href="#" class="flex-c-m p-lr-10 trans-04">
+                        My Account
+                    </a>
 
-						<a href="#" class="flex-c-m p-lr-10 trans-04">
-							EN
-						</a>
+                    <a href="#" class="flex-c-m p-lr-10 trans-04">
+                        EN
+                    </a>
 
-						<a href="#" class="flex-c-m p-lr-10 trans-04">
-							USD
-						</a>
-					</div>
-				</li>
-			</ul>
+                    <a href="#" class="flex-c-m p-lr-10 trans-04">
+                        USD
+                    </a>
+                </div>
+            </li>
+        </ul>
 
-			<ul class="main-menu-m">
-				<li>
-					<a href="index.html">Home</a>
-					<ul class="sub-menu-m">
-						<li><a href="index.html">Homepage 1</a></li>
-						<li><a href="home-02.html">Homepage 2</a></li>
-						<li><a href="home-03.html">Homepage 3</a></li>
-					</ul>
-					<span class="arrow-main-menu-m">
-						<i class="fa fa-angle-right" aria-hidden="true"></i>
-					</span>
-				</li>
+        <ul class="main-menu-m">
+            <li>
+                <a href="index.html">Home</a>
+                <ul class="sub-menu-m">
+                    <li><a href="index.html">Homepage 1</a></li>
+                    <li><a href="home-02.html">Homepage 2</a></li>
+                    <li><a href="home-03.html">Homepage 3</a></li>
+                </ul>
+                <span class="arrow-main-menu-m">
+                    <i class="fa fa-angle-right" aria-hidden="true"></i>
+                </span>
+            </li>
 
-				<li>
-					<a href="product.html">Shop</a>
-				</li>
+            <li>
+                <a href="product.html">Shop</a>
+            </li>
 
-				<li>
-					<a href="shoping-cart.html" class="label1 rs1" data-label1="hot">Features</a>
-				</li>
+            <li>
+                <a href="shoping-cart.html" class="label1 rs1" data-label1="hot">Features</a>
+            </li>
 
-				<li>
-					<a href="blog.html">Blog</a>
-				</li>
+            <li>
+                <a href="blog.html">Blog</a>
+            </li>
 
-				<li>
-					<a href="about.html">About</a>
-				</li>
+            <li>
+                <a href="about.html">About</a>
+            </li>
 
-				<li>
-					<a href="contact.html">Contact</a>
-				</li>
-			</ul>
-		</div>
+            <li>
+                <a href="contact.html">Contact</a>
+            </li>
+        </ul>
+    </div>
 
-		<!-- Modal Search -->
-		<div class="modal-search-header flex-c-m trans-04 js-hide-modal-search">
-			<div class="container-search-header">
-				<button class="flex-c-m btn-hide-modal-search trans-04 js-hide-modal-search">
-					<img src="images/icons/icon-close2.png" alt="CLOSE">
-				</button>
+    <!-- Modal Search -->
+    <div class="modal-search-header flex-c-m trans-04 js-hide-modal-search">
+        <div class="container-search-header">
+            <button class="flex-c-m btn-hide-modal-search trans-04 js-hide-modal-search">
+                <img src="images/icons/icon-close2.png" alt="CLOSE">
+            </button>
 
-				<form class="wrap-search-header flex-w p-l-15">
-					<button class="flex-c-m trans-04">
-						<i class="zmdi zmdi-search"></i>
-					</button>
-					<input class="plh3" type="text" name="search" placeholder="Search...">
-				</form>
-			</div>
-		</div>
-	</header>
+            <form class="wrap-search-header flex-w p-l-15">
+                <button class="flex-c-m trans-04">
+                    <i class="zmdi zmdi-search"></i>
+                </button>
+                <input class="plh3" type="text" name="search" placeholder="Search...">
+            </form>
+        </div>
+    </div>
+</header>
 
-	<script>
-		function cartcount()
-		{
-			$.ajax({
-				type:"get",
-				url:"../ajax/ajax_cart_count.php",
-			}).done(function(data){
-				$("#cartitem").html(data);
-				//console.log(data);
+<script>
+function cartcount() {
+    $.ajax({
+        type: "get",
+        url: "ajax/ajax_cart_count.php",
+    }).done(function(data) {
+        $("#cartitem").html(data);
+        //console.log(data);
 
-			});
-		}
-		$(document).ready(function(){
-			cartcount();
-		});
+    });
+}
+$(document).ready(function() {
+    cartcount();
+});
 
-	
 
-		function wishcount()
-		{
-			$.ajax({
-				type:"get",
-				url:"../ajax/ajax_wish_count.php",
-			}).done(function(data){
-				$("#wishitem").html(data);
-				//console.log(data);
 
-			});
-		}
-		$(document).ready(function(){
-			wishcount();
-		});
-		
-	</script>
+function wishcount() {
+    $.ajax({
+        type: "get",
+        url: "ajax/ajax_wish_count.php",
+    }).done(function(data) {
+        $("#wishitem").html(data);
+        //console.log(data);
+
+    });
+}
+$(document).ready(function() {
+    wishcount();
+});
+</script>
